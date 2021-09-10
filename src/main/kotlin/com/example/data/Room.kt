f3c768f81b183a8b889e9b9b98fdeb46906e8955
Smital Desai <desaismital@gmail.com>
2021-09-10 09:36:05 +0530

    implement the phases aka states

--- src/main/kotlin/com/example/data/Room.kt
+++ src/main/kotlin/com/example/data/Room.kt
@@ -8,6 +8,32 @@ class Room(
     val maxPlayers: Int,
     var players: List<Player> = listOf()
 ) {
+
+    private var phaseChangedListener:((Phase) -> Unit)? = null
+    var currentPhase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            synchronized(field) {
+                field = value
+                phaseChangedListener?.let { it(value) }
+            }
+        }
+
+    private fun setPhaseChangeListener(listener: (Phase) -> Unit) {
+        phaseChangedListener = listener
+    }
+
+    init {
+         setPhaseChangeListener { phase ->
+             when(phase) {
+                 Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                 Phase.WAITING_FOR_START -> waitingForStart()
+                 Phase.NEW_ROUND -> newRound()
+                 Phase.GAME_RUNNING -> gameRunning()
+                 Phase.SHOW_WORD -> showWord()
+             }
+         }
+    }
+
     suspend fun broadcast(message: String) {
         players.forEach {
             if (it.socket.isActive) {
@@ -30,5 +56,34 @@ class Room(
         return players.find {
             it.username == username
         } != null
+    }
+
+
+    private fun waitingForPlayers() {
+
+    }
+
+    private fun waitingForStart() {
+
+    }
+
+    private fun newRound() {
+
+    }
+
+    private fun gameRunning() {
+
+    }
+
+    private fun showWord() {
+
+    }
+
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
